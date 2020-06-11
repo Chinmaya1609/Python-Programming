de9069382d6186d36104cbdb994f7477cdf2771a
Chinmaya1609 <66688101+Chinmaya1609@users.noreply.github.com>
2020-06-11 11:59:21 +0530

    Created program to arrange all positive integers
    This program prints all positive integers in a given list.

---
+++ SortingPositiveInt.py
@@ -0,0 +1,10 @@
+mylist=[12,-7,5,64,-14]
+mylist2=[12,14,-95,3]
+for num in mylist:
+    if num >= 0:
+        print(num,end="  ")
+print('\n')
+for num in mylist2:
+    if num >= 0:
+        print(num,end="  ")
+        
